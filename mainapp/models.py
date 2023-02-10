3e537da280ac4a309e764823e3bf14215024d790
vasiliy <vash050@rmbler.ru>
2023-02-10 10:33:50 +0300

    -models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Hall(models.Model):
+    name = models.CharField(max_length=100)
+    short_description = models.TextField
+    full_description = models.TextField
+
+    class Meta:
+        verbose_name = 'зал'
+        verbose_name_plural = 'залы'
+
+    def __str__(self):
+        return self.name
+
+
+class CategoryService(models.Model):
+    name = models.CharField(max_length=100)
+    short_description = models.TextField
+    full_description = models.TextField
+    hall = models.ForeignKey(to=Hall, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'категория услуг'
+        verbose_name_plural = ' категории услуг'
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    short_description = models.TextField
+    full_description = models.TextField
+    category_service = models.ForeignKey(to=CategoryService, on_delete=models.CASCADE)
+    price = models.DecimalField
+    duration = models.IntegerField  # время на оказание услуги в минутах
+
+    class Meta:
+        verbose_name = 'услуга'
+        verbose_name_plural = 'услуги'
+
+    def __str__(self):
+        return self.name

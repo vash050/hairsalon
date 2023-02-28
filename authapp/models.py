ecaad509909c5d3bc2c0c4ed971f208bf2c63676
altuhov_as <altuhov_as@mail.ru>
2023-02-28 18:39:29 +0300

    Install social-django

--- authapp/models.py
+++ authapp/models.py
@@ -3,15 +3,12 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.core.validators import RegexValidator
-# from masterapp.models import Master
 
 
 class User(AbstractUser):
-    # first_name = models.CharField(_('Имя'), max_length=30, blank=True)
-    # last_name = models.CharField(_('Фамилия'), max_length=50, blank=True)
-    phone_regex = RegexValidator(regex=r"[8]{1}-[0-9]{3}-[0-9]{4}",
-                                 message="Phone number must be entered in the format: '8-999-9999999'. Up to 10 digits allowed.")
-    phone = models.CharField(_('Телефон'), unique=True, validators=[phone_regex], max_length=17, blank=True)  # Validators should be a list
+    phone_regex = RegexValidator(regex=r"[8]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}",
+                                 message="Формат номера телефона должен быть: '8-999-999-9999'. До 10 цифр.")
+    phone = models.CharField(_('Телефон'), unique=True, validators=[phone_regex], max_length=17, blank=True)
     email = models.EmailField(_('Email'), blank=True, unique=True)
     avatar = models.ImageField(upload_to='users', blank=True)
 
@@ -33,6 +30,9 @@ class UserProfile(models.Model):
         ('EM', 'Email'),
     )
     favourite = models.CharField(max_length=10, choices=CHOICES)
+
+    def __str__(self):
+        return '%s %s' % (self.id.__getattribute__('first_name'), self.id.__getattribute__('last_name'))
 
     class Meta:
         verbose_name = 'Профиль пользователя'

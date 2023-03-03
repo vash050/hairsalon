94f75267170847f4385eadef8f5e849b13152569
vasiliy <vash050@rmbler.ru>
2023-03-03 11:56:16 +0300

    - fix master_form.html

--- masterapp/views.py
+++ masterapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, TemplateView
 
-from masterapp.forms import UpdateMasterDetailForm, CompletedWorkCreateForm
+from masterapp.forms import UpdateMasterDetailForm, CompletedWorkCreateForm, UpdateUserForm
 from masterapp.models import CompletedWork, Master
 
 
@@ -14,10 +14,39 @@ class MasterPage(DetailView):
     model = Master
 
 
-class MasterUpdate(UpdateView):
-    model = Master
-    form_class = UpdateMasterDetailForm
-    success_url = reverse_lazy('mainapp:index')
+class MasterUpdate(TemplateView):
+    template_name = 'masterapp/master_form.html'
+    master = None
+
+    def get_master(self, request):
+        self.master = Master.objects.get(user_id_id=request.user.id)
+
+    def get(self, request, *args, **kwargs):
+        self.get_master(request)
+        master_form = UpdateMasterDetailForm(self.request.GET or None,
+                                             instance=self.master)
+        user_form = UpdateUserForm(self.request.GET or None, instance=request.user)
+        context = self.get_context_data(**kwargs)
+        context['master_form'] = master_form
+        context['user_form'] = user_form
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        self.get_master(request)
+        master_form = UpdateMasterDetailForm(request.POST, request.FILES,
+                                             instance=self.master)
+        user_form = UpdateUserForm(request.POST, request.FILES, instance=request.user)
+        if master_form.is_valid() and user_form.is_valid():
+            master_form.save()
+            user_form.save()
+            return render(request, "mainapp/index.html")
+        else:
+            return self.render_to_response(
+                self.get_context_data(
+                    master_form=master_form.errors,
+                    user_form=user_form.errors
+                )
+            )
 
 
 class CompletedWorkCreate(CreateView):

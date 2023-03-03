ad2eb7ac3d2d2cc57666b2a0e080a087463ee4ad
vasiliy <vash050@rmbler.ru>
2023-03-03 11:56:16 +0300

    - fix master_form.html

--- masterapp/forms.py
+++ masterapp/forms.py
@@ -1,6 +1,19 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Form, ChoiceField, CheckboxSelectMultiple, ModelMultipleChoiceField, \
+    ModelChoiceField
 
+from authapp.models import User
 from masterapp.models import Master, CompletedWork
+
+
+class UpdateUserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'avatar', 'phone', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "input-form"
 
 
 class UpdateMasterDetailForm(ModelForm):

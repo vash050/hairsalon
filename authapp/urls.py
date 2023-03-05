3e70c23ef9da3113d0e52c8fafdb0fdda3e26eb5
altuhov_as <altuhov_as@mail.ru>
2023-03-05 21:19:31 +0300

    Create register form

--- authapp/urls.py
+++ authapp/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from authapp.apps import AuthappConfig
 from django.urls import path
-from authapp.views import RegisterView, LogoutView, UserEditView, LoginView, UserDetailView
+from authapp.views import CustomLoginView, RegisterView, LogoutView, UserEditView, UserDetailView
 
-app_name = 'authapp'
+app_name = AuthappConfig.name
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('update/<int:pk>/', UserEditView.as_view(), name='edit'),
+    path('edit/<int:pk>/', UserEditView.as_view(), name='edit'),
     path('detail/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, HttpResponseRedirect
-from django.views.generic import TemplateView, UpdateView, DetailView
-from .models import User, UserProfile
+from django.views.generic import TemplateView, UpdateView, DetailView, CreateView
+from .models import User
 from django.contrib import messages
 from django.urls import reverse, reverse_lazy
+from authapp.forms import CustomUserCreationForm
 
 
-class LoginView(LoginView):
+class CustomLoginView(LoginView):
     redirect_authenticated_user = True
     extra_context = {
         'title': 'Вход пользователя'
@@ -14,50 +15,10 @@ class LoginView(LoginView):
     # success_url = reverse_lazy('mainapp:index')
 
 
-class RegisterView(TemplateView):
-    redirect_authenticated_user = True
-    template_name = 'authapp/register.html'
-    extra_context = {
-        'title': 'Регистрация пользователя'
-    }
-
-    def post(self, request, *args, **kwargs):
-        print(request.POST)
-        try:
-            if all(
-                    (
-                            request.POST.get('phone'),
-                            request.POST.get('email'),
-                            request.POST.get('password1'),
-                            request.POST.get('password2'),
-                            request.POST.get('first_name'),
-                            request.POST.get('last_name'),
-                            request.POST.get('password1') == request.POST.get('password2')
-                    )
-            ):
-                new_user = User.objects.create(
-                    username=request.POST.get('phone'),
-                    phone=request.POST.get('phone'),
-                    first_name=request.POST.get('first_name'),
-                    last_name=request.POST.get('last_name'),
-                    email=request.POST.get('email'),
-                    avatar=request.FILES.get('avatar')
-                )
-                print(new_user)
-                new_user.set_password(request.POST.get('password1'))
-                new_user.save()
-                messages.add_message(request, messages.INFO, 'Регистрация прошла успешно')
-                return HttpResponseRedirect(reverse('authapp:login'))
-            else:
-                messages.add_message(request,
-                                     messages.WARNING,
-                                     'Что-то пошло не так.')
-                return HttpResponseRedirect(reverse('authapp:register'))
-        except Exception as ex:
-            messages.add_message(request,
-                                 messages.WARNING,
-                                 'Что-то пошло не так.')
-            return HttpResponseRedirect(reverse('mainapp:index'))
+class RegisterView(CreateView):
+    model = User
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('mainapp:index')
 
 
 class UserEditView(UpdateView):
